a59138d9f9d46f7d7335a7564a79fa74494fa684
Natraj Yegnaraman <natraj@dreamingincrm.com>
2020-05-30 15:11:21 +1000

    Initial commit

---
+++ EasyReproDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EasyReproDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EasyReproDemo")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9efaf7e5-dc7b-4e32-a336-517dee6a4e5f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EasyReproDemo/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Security;
+using Microsoft.Dynamics365.UIAutomation.Api.UCI;
+using Microsoft.Dynamics365.UIAutomation.Browser;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EasyReproDemo
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private static TestContext _testContext;
+        private static SecureString _username;
+        private static SecureString _password;
+        private static Uri _appUrl;
+
+        [ClassInitialize]
+        public static void Initialize(TestContext testContext)
+        {
+            _testContext = testContext;
+
+            _username = _testContext.Properties["username"].ToString().ToSecureString();
+            _password = _testContext.Properties["password"].ToString().ToSecureString();
+            _appUrl = new Uri(_testContext.Properties["appurl"].ToString());
+
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var client = new WebClient(new BrowserOptions
+            {
+                EnableRecording = false,
+                BrowserType = BrowserType.Chrome,
+                PrivateMode = true,
+                PageLoadTimeout = TimeSpan.FromSeconds(2),
+            });
+            using (var xrmApp = new XrmApp(client))
+            {
+                xrmApp.OnlineLogin.Login(_appUrl, _username, _password);
+
+                xrmApp.Navigation.OpenApp("Customer Service Hub");
+
+                xrmApp.Navigation.OpenSubArea("Service", "Accounts");
+
+            }
+        }
+    }
+}
