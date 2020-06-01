dd1154addfb6d68befa725c0a2748d126fe99953
Natraj Yegnaraman <natraj@dreamingincrm.com>
2020-06-02 05:48:45 +1000

    Finalise EasyRepro

--- EasyReproDemo/CustomerServiceHubTests.cs
+++ EasyReproDemo/CustomerServiceHubTests.cs
@@ -9,16 +9,17 @@ namespace EasyReproDemo
     public class CustomerServiceHubTests
     {
         private static XrmApp _xrmApp;
+        private static WebClient _webClient;
 
         [ClassInitialize]
         public static void Initialize(TestContext testContext)
         {
-            var username = Environment.GetEnvironmentVariable("username")?.ToSecureString() ?? testContext.Properties["username"].ToString().ToSecureString();
-            var password = Environment.GetEnvironmentVariable("password")?.ToSecureString() ?? testContext.Properties["password"].ToString().ToSecureString();
+            var username = Environment.GetEnvironmentVariable("powerappsusername")?.ToSecureString() ?? testContext.Properties["powerappsusername"].ToString().ToSecureString();
+            var password = Environment.GetEnvironmentVariable("powerappspassword")?.ToSecureString() ?? testContext.Properties["powerappspassword"].ToString().ToSecureString();
             var appUrl = new Uri(Environment.GetEnvironmentVariable("appurl")?.ToString() ?? testContext.Properties["appurl"].ToString());
             var mfa = Environment.GetEnvironmentVariable("mfaToken")?.ToString().ToSecureString() ?? testContext.Properties["mfaToken"].ToString().ToSecureString();
 
-            var client = new WebClient(new BrowserOptions
+            _webClient = new WebClient(new BrowserOptions
             {
                 EnableRecording = true,
                 BrowserType = BrowserType.Chrome,
@@ -26,7 +27,7 @@ namespace EasyReproDemo
                 UCITestMode = false,
                 Headless = bool.Parse(testContext.Properties["headless"]?.ToString() ?? "true")
             });
-            _xrmApp = new XrmApp(client);
+            _xrmApp = new XrmApp(_webClient);
             _xrmApp.OnlineLogin.Login(appUrl, username, password, mfa);
 
         }
@@ -46,11 +47,21 @@ namespace EasyReproDemo
         }
 
         [TestMethod]
-        public void Check_If_Show_Chart_Button_Exists()
+        public void QuickCreate_Contact()
         {
+            _xrmApp.Navigation.QuickCreate("contact");
+            _xrmApp.QuickCreate.SetValue("firstname", "Easy");
+            _xrmApp.QuickCreate.SetValue("lastname", "Repro");
+            _xrmApp.QuickCreate.Save();
             _xrmApp.Navigation.OpenSubArea("Service", "Contacts");
-            var commandBarButtons = _xrmApp.CommandBar.GetCommandValues();
-            Assert.IsTrue(commandBarButtons.Value.Contains("Show Chart"));
+            _xrmApp.Grid.Search("Easy");
+            var contacts = _xrmApp.Grid.GetGridItems();
+            Assert.IsTrue(contacts.Count > 0);
+
+            _xrmApp.Grid.OpenRecord(0);
+            _webClient.Browser.Driver.ExecuteScript("Xrm.WebApi.deleteRecord('contact', Xrm.Utility.getPageContext().input.entityId).then(x=>Xrm.Navigation.navigateTo({pageType: 'entitylist', entityName: 'contact'}))");
+            _xrmApp.Grid.Search("Easy");
+            Assert.IsTrue(_xrmApp.Grid.GetGridItems().Count == 0);
         }
     }
 }
